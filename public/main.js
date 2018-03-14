e30550dd00ee58c9a4e0c07750002ff32ef9494f
Tiller <ctiller@proforma.com>
2018-03-14 08:40:31 -0400

    create button and add automatic timer to function

--- public/main.js
+++ public/main.js
@@ -1,5 +1,32 @@
+const background = document.querySelector("body");
+const colorText = document.querySelector("h1");
+const colorButton = document.querySelector(".colorChange");
+
+const colorChange = () => {
+  let red = calculateColor();
+  let blue = calculateColor();
+  let green = calculateColor();
+  // console.log(red, blue, green);
+  let colorName = `rgb(${red},${green},${blue})`;
+  let invertedColor = `rgb(${255 - red},${255 - green},${255 - blue})`;
+
+  colorText.textContent = colorName;
+  colorText.style.color = invertedColor;
+  background.style.backgroundColor = colorName;
+  colorButton.style.backgroundColor = invertedColor;
+  colorButton.style.color = colorName;
+
+}
+
+const calculateColor = () => {
+  return Math.floor(Math.random() * 256);
+}
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  let colorChangingAuto = setInterval(() => {
+    colorChange();
+  }, 10000);
+
 }
 
 document.addEventListener('DOMContentLoaded', main)
